f9f2990366666374435933c9a29db160408d429f
gogowonji <65698313+gogowonji@users.noreply.github.com>
2024-06-05 10:21:10 +0900

    [level 2] Title: 더 맵게, Time: 1649.80 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,33 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heap = []
+    for s in scoville:
+        heapq.heappush(heap,s)
+    
+    while heap[0] < K:
+        heapq.heappush(heap, heapq.heappop(heap) + heapq.heappop(heap)*2)
+        answer += 1
+        if len(heap) == 1 and heap[0] < K:
+            return -1
+    
+    # for s in range(len(scoville)): #scoville 값이 계속 변하는데..?!
+    #     scoville.sort()
+    #     if scoville[s] >= K:
+    #         break
+    #     else:
+    #         scoville.append(scoville[s] + scoville[s+1]*2)
+    #         scoville.pop(0) # 앞 두개 제거
+    #         scoville.pop(1)
+    #         answer += 1
+    #         s = 0
+    return answer
+
+# 정렬
+# for
+# 배열의 맨 앞 숫자가 K이상 인지 확인
+# 앞 두개 섞어 새로운 음식
+# 제거
+# 새로운 음식 만드는 갯수만 반환하면 되니까 다른 배열 만들어?
+
+
