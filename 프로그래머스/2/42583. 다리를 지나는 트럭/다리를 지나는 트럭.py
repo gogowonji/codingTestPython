ec031389319a6c1f0122cea9fea50748ad335a6f
gogowonji <65698313+gogowonji@users.noreply.github.com>
2024-06-10 13:59:37 +0900

    복습 다리를 지나는 트럭.py

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -5,7 +5,6 @@ def solution(bridge_length, weight, truck_weights):
     trucks = deque(truck_weights)
     curWeight = 0
     time = 0
-    print(f"bridge : {bridge}")
     while bridge:
             time += 1
             curWeight -= bridge.popleft()
@@ -23,15 +22,25 @@ def solution(bridge_length, weight, truck_weights):
     return time
 
 # 개수와 무게 제한을 어디에 해야 할까..
-# len(temp) <= length and temp 총 값 <= weight
+# -> 다리에 올라가는 트럭이 모두 사라질때까지..! if문으로
+
+# if len(temp) <= bridge_length and sum(temp) <= weight:
+# -> 다리에 올라가는 트럭 deque를 따로 만들기 and 현재 무게를 계산하도록 변수 생성 후 if문
+
 # 제한 걸고 temp push, truck_weights에서 pop
 # pop 여러번 해야 하는 경우 - 제한 ㄱㅊ으면 루프 돌게
+# -> 여러번 해야 하는 경우 x <- 시간마다 pop하는 거니까 트럭을 다리에 올리기 전 다리 올라간 트럭들 pop해주면 되는 거였음
+
 # 루프 몇번 도는지 셀때 answer++
 # 트럭 pop도 해주면서 answer++?
-# if len(temp) <= bridge_length and sum(temp) <= weight:
-            #     for t in range(len(truck_weights)):
-            #         temp.append(truck_weights.pop(0))
-            # temp.pop(0)
+# -> 1대 이상 있고, 이미 무게를 초과한 경우
+# -> bridge.append(0)로 무게 초과한 경우에도 대수 제한 유지 + 시간은 제대로 흐를 수 있게
 
-# deque 학습 필요
+
+# POINT
+# deque
+# 다리 위에 올라가는 트럭 deque
+# 대기 중인 트럭 deque
+# 현재 무게 저장 변수
+# 무게 초과시, 다리 위에 올라가는 트럭 deque에 0 push하기
 
