85c970d488c58f801be18fc526dafe2d71732293
gogowonji <65698313+gogowonji@users.noreply.github.com>
2024-06-10 22:28:25 +0900

    복습 조건에 부합하는 중고거래 댓글 조회하기.sql

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,4 +1,7 @@
--- 코드를 입력하세요
+-- 다른 테이블에 동일한 컬럼명이 있을 수 있다.
+-- 어떤 값을 원하는지 문제를 꼼꼼히 보기
+-- date_format 주의
+-- 정렬 우선순위 쉼표로 표시 가능
 SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, 
     r.WRITER_ID, r.CONTENTS, date_format(r.CREATED_DATE,'%Y-%m-%d') as CREATED_DATE
 from USED_GOODS_BOARD b
