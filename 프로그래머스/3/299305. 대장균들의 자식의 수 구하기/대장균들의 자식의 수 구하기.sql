f7e3bea2420b5a9d211359b49f4e9ec48d187f0d
gogowonji <65698313+gogowonji@users.noreply.github.com>
2024-06-11 11:37:00 +0900

    복습 대장균들의 자식의 수 구하기.sql

--- 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -1,6 +1,6 @@
--- 코드를 작성해주세요
+-- 서브 쿼리 사용
+-- HAVING (ID = PARENT_ID) 주의 <- 이걸로 ID 별 자식수 가져올 수 있었음
 -- 부모 ID를 가진 놈들이 자식
--- parent_id이 없는 놈은 부모
 SELECT ID, IFNULL((SELECT COUNT(*)
                   FROM ECOLI_DATA
                   GROUP BY PARENT_ID
