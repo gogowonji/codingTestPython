693dcae5a2ad984d4692a921f5707ef3eb734cdc
gogowonji <65698313+gogowonji@users.noreply.github.com>
2024-06-12 16:41:39 +0900

    Update H－Index.py

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,16 +1,12 @@
-# 정렬 문제라고 하니까..
+# h-index 개념이 이해가 안됐음 -> 리트코드 참고
+
 # 정렬 후 중앙값 찾기
+# -> 중앙값을 찾는 문제인줄 알았음 (아니었음)
 # 정렬 - 시간 복잡도..
 # index
 # 홀수개 len/2 + 1
 # 짝수개 len/2
-# answer = 0
-# index = 0
-# citations.sort()
-# print(citations)
-# index = (int)(len(citations)/2)
-# answer = citations[index]
-# return answer
+
 def solution(citations):
     answer = 0
     
