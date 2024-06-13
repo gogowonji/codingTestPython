07f4f807d1297331447e22305bb528cc0bd0df13
gogowonji <65698313+gogowonji@users.noreply.github.com>
2024-06-13 15:15:52 +0900

    복습 물고기 종류 별 대어 찾기.sql

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,4 +1,4 @@
--- 코드를 작성해주세요
+-- 서브 쿼리, 다중값 IN 좋다! 배워가기
 SELECT f.ID, fn.FISH_NAME, f.LENGTH
 FROM FISH_INFO f
 INNER JOIN FISH_NAME_INFO fn ON f.FISH_TYPE = fn.FISH_TYPE
@@ -6,3 +6,4 @@ WHERE (f.FISH_TYPE, f.LENGTH) IN (SELECT FISH_TYPE, MAX(LENGTH)
                             FROM FISH_INFO
                             GROUP BY FISH_TYPE)
 ORDER BY f.ID;
+-- ORDER BY 1; -- 해도 됨
