660f4c8a3e0383a75e2c816db1b0734ab97e4369
gogowonji <65698313+gogowonji@users.noreply.github.com>
2024-06-19 15:48:59 +0900

    복습 단어 공부.py

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,7 +1,18 @@
+# ord()
+# enumerate(), 리스트컴프리헨션
+# 여러번 세어진 것을 구하는 부분이 헷갈렸음
+
+# print(ord('a'))  #97
+# print(ord('A'))  #65
+# print(ord('z')) #122
+# print(ord('Z')) #90
+
+
 s = str(input())
 answer = ''
 count = [0] * 26
 max_count = 0
+
 for i in s:
   if (ord(i) >= ord('a')):  #소문자이면
     count[ord(i) - ord('a')] += 1
