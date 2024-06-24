bb40241f995973bf2e0fb781eb0beb32504c01b1
gogowonji <65698313+gogowonji@users.noreply.github.com>
2024-06-24 09:08:49 +0900

    복습 색종이.py

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,20 +1,21 @@
-# 도화지의 크기를 정의합니다.
-DOHWAJI_SIZE = 100
+# 좌표 영역 2차원 배열로 받아오는 아이디어!
+# 겹쳐진 영역, 안 겹쳐진 영역 상관 없이 1 넣기
+# 행마다 sum하기
 
-# 도화지를 2D 배열로 초기화합니다.
+# 도화지 크기
+DOHWAJI_SIZE = 100
+# 좌표 영역을 2차원 배열로 저장
 dohwaji = [[0] * DOHWAJI_SIZE for _ in range(DOHWAJI_SIZE)]
 
-# 색종이의 수를 입력받습니다.
 n = int(input())
 
-# 색종이 위치를 입력받아 도화지에 붙입니다.
 for _ in range(n):
     x, y = map(int, input().split())
     for i in range(x, x + 10):
         for j in range(y, y + 10):
             dohwaji[i][j] = 1
 
-# 검은 영역의 넓이를 계산합니다.
+# 검은 영역 줄마다..
 black_area = sum(sum(row) for row in dohwaji)
 
 print(black_area)
