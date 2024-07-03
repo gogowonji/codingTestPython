b783044e67e7fa0b008eaa5a2074ad5d4bed2431
gogowonji <65698313+gogowonji@users.noreply.github.com>
2024-07-03 14:30:00 +0900

    [Silver II] Title: 촌수계산, Time: 52 ms, Memory: 34060 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,68 @@
+# 2644 촌수 계산
+# n : 전체 사람의 수 (그래프 행수)
+# p1, p2 : 촌수를 계산해야 하는 서로 다른 두사람의 번호
+# m : 부모 자식간의 관계의 개수
+# x,y : 부모 자식간의 관계를 나타내는 두 번호 (m번 만큼)
+from collections import deque
+
+def bfs(graph, start, target, visited):
+    queue = deque([(start,0)])
+    visited[start] = True
+    # dfs로 풀라 그러다가..
+    # for _ in graph[visited]:
+    # if not visited[i]:
+    #     if i == p2:
+    #         return
+    #     else:
+    #         # 어떻게 처리하지..
+    while queue:
+        current, count = queue.popleft()
+        if current == target:
+            return count
+        for neighbor in graph[current]:
+            if not visited[neighbor]:
+                visited[neighbor] = True
+                queue.append((neighbor, count+1))
+    return -1
+
+
+
+
+def main():
+    n = int(input())
+    # n+1 만큼 리스트 만들어 주기 (0이 없음)
+    graph = [[] for _ in range(n+1)]
+
+    p1, p2 = map(int, input().split())
+    m = int(input())
+
+    # 해당 되는 부모에 값 넣어주기~
+    for _ in range(m):
+        x,y = map(int, input().split())
+        graph[x].append(y)
+        graph[y].append(x)
+
+    # 방문했나 안했나
+    visited = [False]*(n+1)
+
+
+    # dfs 사용해보기..였는데
+    # 값 찾을때마다 count++?
+    result = bfs(graph,p1,p2,visited)
+    print(result)
+
+    # result = 0
+    # for b in visited:
+    #     print(b)
+    #     if b:
+    #         result += 1
+
+    # print(result)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
